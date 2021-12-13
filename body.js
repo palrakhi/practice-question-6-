d8ae0f65fc48509aea6d03cf4df4420ae075c6c8
palrakhi <69839457+palrakhi@users.noreply.github.com>
2021-12-13 13:08:14 +0530

    Add files via upload

---
+++ body.js
@@ -0,0 +1,21 @@
+var loadBtn= document.querySelector("#load");
+var showOutput= document.querySelector("#output")
+
+
+
+
+loadBtn.addEventListener("click", showLoading)
+
+function showLoading(){
+    showOutput.style.display="none"
+
+}
+
+//     if(loadBtn.addEventListener("click", )){
+//         showOutput.style.display= "none"
+//     } else {
+//         showOutput.innerText= "Loding...."
+//     }
+// }
+
+
